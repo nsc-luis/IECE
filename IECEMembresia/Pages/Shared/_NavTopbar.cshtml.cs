2890bc957b565b674694f8af3362d98450a76a96
nsc_luis <nsc_luis@nscco.com.mx>
2020-09-23 22:59:28 -0500

    Cambios antes de uso de partials

---
+++ IECEMembresia/Pages/Shared/_NavTopbar.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IECEMembresia.Pages.Shared
+{
+    public class _NavTopbarModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
