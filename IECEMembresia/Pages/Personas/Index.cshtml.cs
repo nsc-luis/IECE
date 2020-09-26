834d2432e63b4baa9656ae384825fc5af95cf18b
nsc_luis <nsc_luis@nscco.com.mx>
2020-09-26 02:06:07 -0500

    Adicion de formulario para altas

--- IECEMembresia/Pages/Personas/Index.cshtml.cs
+++ IECEMembresia/Pages/Personas/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace IECEMembresia.Pages.Personal
+namespace IECEMembresia.Pages.Personas
 {
     public class IndexModel : PageModel
     {
