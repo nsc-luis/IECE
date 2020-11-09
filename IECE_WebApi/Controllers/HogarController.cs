598bcf201dfdd06496accb4652682e1bc981877a
nsc_luis <nsc_luis@nscco.com.mx>
2020-11-09 01:11:07 -0600

    adicion de documentacion con nuget Swagger

--- IECE_WebApi/Controllers/HogarController.cs
+++ IECE_WebApi/Controllers/HogarController.cs
@@ -50,7 +50,7 @@ namespace IECE_WebApi.Controllers
         /// <returns>JSON con lista de hogares</returns>
         /// <response code="201">Retorna JSON de lista de hogares</response>
         /// <response code="400">Retorna JSON con mensaje de error</response> 
-[HttpGet]
+        [HttpGet]
         [EnableCors("AllowOrigin")]
         public ActionResult<IEnumerable<Hogar>> Get()
         {
@@ -60,12 +60,6 @@ namespace IECE_WebApi.Controllers
             }
             catch (Exception ex)
             {
-                /* turn BadRequest(
-                        new object []
-                        {
-                            new { error = "Error: aun no hay domicilios guardados"}
-                        }
-                    ); */
                 return BadRequest(ex.Message);
             }
         }
@@ -84,6 +78,36 @@ namespace IECE_WebApi.Controllers
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
+            }
+        }
+
+        // GET: api/Hogar/GetMiembros/2
+        [Route("[action]/{hog_Relacion_Hogar_Persona}")]
+        [HttpGet]
+        [EnableCors("AllowOrigin")]
+        public IActionResult GetMiembros(int hog_Relacion_Hogar_Persona)
+        {
+            try
+            {
+                var query = (from h in context.Hogar
+                    join p in context.Persona
+                    on h.per_Id_Persona equals p.per_Id_Persona
+                    where h.hog_Relacion_Hogar_Persona == hog_Relacion_Hogar_Persona
+                    select new
+                    {
+                        hog_Id_Hogar = h.hog_Id_Hogar,
+                        hog_Relacion_Hogar_Persona = h.hog_Relacion_Hogar_Persona,
+                        hog_Jerarquia = h.hog_Jerarquia,
+                        per_Id_Persona = p.per_Id_Persona,
+                        per_Nombre = p.per_Nombre,
+                        per_Apellido_Paterno = p.per_Apellido_Paterno,
+                        per_Apellido_Materno = p.per_Apellido_Materno
+                    }).ToList();
+                return Ok(query);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
             }
         }
 
