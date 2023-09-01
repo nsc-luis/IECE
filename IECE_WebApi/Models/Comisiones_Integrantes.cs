81304acd730e7672c1ccc46e3cc2d23fb3884804
Luis Gerardo <luis_gera_rdz@hotmail.com>
2023-08-31 23:56:34 -0600

    31/08/2023

---
+++ IECE_WebApi/Models/Comisiones_Integrantes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IECE_WebApi.Models
+{
+    public class Comisiones_Integrantes
+    {
+        public int ci_Id_ComisionIntegrante { get; set; }
+        public int com_Id_Comision { get; set; }
+        public int per_Id_Persona { get; set; }
+        public DateTime ci_Fecha_Ingreso { get; set; }
+        public int pem_Id_Ministro { get; set; }
+    }
+}
