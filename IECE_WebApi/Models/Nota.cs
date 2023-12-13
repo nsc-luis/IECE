7f26d69fdb994935dff958845deb90ea1130493c
Luis Gerardo <luis_gera_rdz@hotmail.com>
2023-12-13 00:35:20 -0600

    actualizacion al 13/12/2023

---
+++ IECE_WebApi/Models/Nota.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IECE_WebApi.Models
+{
+    public class Nota
+    {
+        [Key]
+        public int N_Id { get; set; }
+        public int Vp_Id_Visitante { get; set; }
+        public string N_Nota { get; set; }
+        public DateTime N_Fecha_Nota { get; set; }
+        public DateTime? Fecha_Registro { get; set; }
+        public int Usu_Id_Usuario { get; set; }
+    }
+}
---
+++ IECE_WebApi/Models/Visitante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace IECE_WebApi.Models
+{
+    public class Visitante
+    {
+        [Key]
+        public int Vp_Id_Visitante { get; set; }
+        public bool? Vp_Activo { get; set; }
+        public int Vp_Numero_Lista { get; set; }
+        public string Vp_Nombre { get; set; }
+        public string Vp_Telefono_Contacto { get; set; }
+        public string Vp_Direccion { get; set; }
+        public string Vp_Tipo_Visitante { get; set; }
+        public int sec_Id_Sector { get; set; }
+        public DateTime? Fecha_Registro { get; set; }
+        public int Usu_Id_Usuario { get; set; }
+    }
+}
