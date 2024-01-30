c51a25c9738736dabf7eca422bc92a40fcd14f92
Luis Gerardo <luis_gera_rdz@hotmail.com>
2024-01-30 10:09:15 -0600

    actualizacion al 30/01/2024

--- IECE_WebApi/Controllers/VisitanteController.cs
+++ IECE_WebApi/Controllers/VisitanteController.cs
@@ -42,6 +42,118 @@ namespace IECE_WebApi.Controllers
             public DateTime n_Fecha_Nota { get; set; }
         }
 
+        private int OrdenaPrioridad(int idVisitante, int idSector, int prioridad)
+        {
+            /*
+            if == 0
+                if visitantes < 1
+                    se asigna 1
+                else 
+                    if == 1
+                        se asigna 1 y los demas se incrementan en 1
+                    if != 1
+                        se asigna como el ultimo de prioridad
+            else != 0
+                if visitantes < 1
+                    se asigna 1
+                else visitantes > 0
+                    se asigna prioridad seleccionada
+                    se consulta a partir de numero asignado y se incrementan en 1
+            */
+            var visitantes = context.Visitante.Where(v => v.sec_Id_Sector == idSector)
+                .OrderBy(v => v.vp_Numero_Lista)
+                .ToList();
+            int numeroDeLista = 0;
+            if (prioridad == 0)
+            {
+                if (visitantes.Count == 0)
+                {
+                    numeroDeLista = 1;
+                }
+                else
+                {
+                    if (prioridad == 1)
+                    {
+                        numeroDeLista = 1;
+                        foreach (var frequenter in visitantes)
+                        {
+                            frequenter.vp_Numero_Lista++;
+                            context.Visitante.Update(frequenter);
+                            context.SaveChanges();
+                        }
+                    }
+                    else
+                    {
+                        numeroDeLista = visitantes.Count + 1;
+                    }
+                }
+            }
+            else
+            {
+                if (visitantes.Count == 0)
+                {
+                    numeroDeLista = 1;
+                }
+                else if (prioridad == 1)
+                {
+                    numeroDeLista = 1;
+                    foreach (var frequenter in visitantes)
+                    {
+                        frequenter.vp_Numero_Lista++;
+                        context.Visitante.Update(frequenter);
+                        context.SaveChanges();
+                    }
+                }
+                else if (idVisitante == 0)
+                {
+                    numeroDeLista = prioridad;
+                    var frequenters = context.Visitante
+                        .Where(v => v.vp_Numero_Lista >= prioridad && v.sec_Id_Sector == idSector)
+                        .OrderBy(v => v.vp_Numero_Lista)
+                        .ToList();
+                    foreach (var frequenter in frequenters)
+                    {
+                        frequenter.vp_Numero_Lista++;
+                        context.Visitante.Update(frequenter);
+                        context.SaveChanges();
+                    }
+                }
+                else
+                {
+                    numeroDeLista = prioridad;
+                    var visitante = context.Visitante.FirstOrDefault(v => v.vp_Id_Visitante == idVisitante);
+                    
+                    if (prioridad  < visitante.vp_Numero_Lista)
+                    {
+                        // suman 1 en el rango
+                        var eVisitantes = visitantes
+                        .Where(v => v.vp_Numero_Lista >= prioridad && v.vp_Numero_Lista <= visitante.vp_Numero_Lista)
+                        .ToList();
+                        foreach (var eVisitante in eVisitantes)
+                        {
+                            eVisitante.vp_Numero_Lista++;
+                            context.Visitante.Update(eVisitante);
+                            context.SaveChanges();
+                        }
+                    }
+                    else
+                    {
+                        // restan 1 en el rango
+                        var eVisitantes = visitantes
+                        .Where(v => v.vp_Numero_Lista >= visitante.vp_Numero_Lista && v.vp_Numero_Lista <= prioridad)
+                        .ToList();
+                        foreach (var eVisitante in eVisitantes)
+                        {
+                            eVisitante.vp_Numero_Lista--;
+                            context.Visitante.Update(eVisitante);
+                            context.SaveChanges();
+                        }
+                    }
+                }
+            }
+            return numeroDeLista;
+        }
+
         [HttpGet]
         [Route("[action]/{sec_Id_Sector}")]
         [EnableCors("AllowOrigin")]
@@ -49,7 +161,10 @@ namespace IECE_WebApi.Controllers
         {
             try
             {
-                var visitantes = context.Visitante.Where(v => v.sec_Id_Sector == sec_Id_Sector && v.vp_Activo == true).ToList();
+                var visitantes = context.Visitante
+                    .Where(v => v.sec_Id_Sector == sec_Id_Sector /*&& v.vp_Activo == true*/)
+                    .OrderBy(v => v.vp_Numero_Lista)
+                    .ToList();
                 return Ok(new
                 {
                     status = "success",
@@ -79,7 +194,8 @@ namespace IECE_WebApi.Controllers
                     visitante
                 });
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 return Ok(new
                 {
                     status = "error",
@@ -101,18 +217,12 @@ namespace IECE_WebApi.Controllers
                                 orderby vp.vp_Numero_Lista ascending
                                 select vp).ToList();
 
-                if (contador.Count < 1) { visitante.vp_Numero_Lista = 1; }
-                if (contador.Count != visitante.vp_Numero_Lista)
-                {
-                    for (int i = visitante.vp_Numero_Lista; i <= contador.Count; i++)
-                    {
-
-                    }
-                }
-
                 visitante.vp_Activo = true;
                 visitante.Fecha_Registro = fechayhora;
-                visitante.vp_Numero_Lista = contador.Count > 0 ? contador.LastOrDefault().vp_Numero_Lista + 1 : 1;
+                // ordena las priordades segun seleccion
+                visitante.vp_Numero_Lista = OrdenaPrioridad(visitante.vp_Id_Visitante, visitante.sec_Id_Sector, visitante.vp_Numero_Lista);
+
+                // alta de visitante
                 context.Visitante.Add(visitante);
                 context.SaveChanges();
 
@@ -145,13 +255,20 @@ namespace IECE_WebApi.Controllers
             }
         }
 
-        [HttpPut]
+        [HttpPut("{vp_Id_Visitante}")]
         [EnableCors("AllowOrigin")]
-        public IActionResult Put([FromBody] Visitante visitante)
+        public IActionResult Put([FromBody] Visitante visitante, int vp_Id_Visitante)
         {
             try
             {
-                context.Entry(visitante).State = EntityState.Modified;
+                var editaVisitante = context.Visitante.FirstOrDefault(v => v.vp_Id_Visitante == vp_Id_Visitante);
+                editaVisitante.vp_Tipo_Visitante = visitante.vp_Tipo_Visitante;
+                editaVisitante.vp_Direccion = visitante.vp_Direccion;
+                editaVisitante.vp_Nombre = visitante.vp_Nombre;
+                editaVisitante.Fecha_Registro = visitante.Fecha_Registro;
+                editaVisitante.vp_Telefono_Contacto = visitante.vp_Telefono_Contacto;
+                editaVisitante.vp_Numero_Lista = OrdenaPrioridad(editaVisitante.vp_Id_Visitante, visitante.sec_Id_Sector, visitante.vp_Numero_Lista);
+                context.Visitante.Update(editaVisitante);
                 context.SaveChanges();
 
                 return Ok(new
@@ -178,6 +295,7 @@ namespace IECE_WebApi.Controllers
             try
             {
                 var visitante = context.Visitante.FirstOrDefault(vp => vp.vp_Id_Visitante == bv.vp_Id_Visitante);
+                visitante.vp_Numero_Lista = OrdenaPrioridad(visitante.vp_Id_Visitante, visitante.sec_Id_Sector, context.Visitante.Count());
                 visitante.vp_Activo = false;
                 context.Visitante.Update(visitante);
                 context.SaveChanges();
